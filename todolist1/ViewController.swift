24fea10bb0e8b5a139d69e5e94204b895c501285
haoshizhong0415@hotmail.com <haoshizhong0415@hotmail.com>
2018-10-08 04:39:52 -0700

    Initial Commit

---
+++ todolist1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todolist1
+//
+//  Created by Shizhong Hao on 10/8/18.
+//  Copyright © 2018 Shizhong Hao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
